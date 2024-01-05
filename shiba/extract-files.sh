5b5f00c558005718f46a91ceb14e3d3289e856a6
Kazuki H <kazukih0205@gmail.com>
2024-01-05 12:55:59 +0900

    Drop carriersettings

--- shiba/extract-files.sh
+++ shiba/extract-files.sh
@@ -63,7 +63,6 @@ function prepare_firmware() {
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
 extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
-extract "${MY_DIR}/proprietary-files-carriersettings.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 extract "${MY_DIR}/proprietary-files-vendor.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 
 if [ -z "${SECTION}" ]; then
