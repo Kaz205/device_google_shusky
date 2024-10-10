205047100be9c250e15195d8af56c879c68e8a04
Kazuki Hashimoto <kazukih0205@gmail.com>
2024-10-10 15:37:54 +0900

    Revert "Drop carriersettings"
    This reverts commit 5b5f00c558005718f46a91ceb14e3d3289e856a6.

--- shiba/extract-files.sh
+++ shiba/extract-files.sh
@@ -71,6 +71,7 @@ function blob_fixup() {
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
 extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+extract "${MY_DIR}/proprietary-files-carriersettings.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 extract "${MY_DIR}/proprietary-files-vendor.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 
 if [ -z "${SECTION}" ]; then
