d83d6e26a58cb838a16e63cbeb4c1547a6b76a0e
Kazuki Hashimoto <kazukih0205@gmail.com>
2024-12-05 04:13:08 +0900

    Add shiba makefile

---
+++ statix_shiba.mk
@@ -0,0 +1,22 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+
+# Inherit some common StatiX stuff.
+$(call inherit-product, vendor/statix/config/common.mk)
+$(call inherit-product, vendor/statix/config/gsm.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/shusky/aosp_shiba.mk)
+
+# Parts
+$(call inherit-product-if-exists, vendor/google/pixelparts/pixelparts.mk)
+$(call inherit-product, vendor/google/pixelparts/face/face.mk)
+$(call inherit-product-if-exists, vendor/google/pixelparts/powershare/device.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := statix_shiba
+PRODUCT_MODEL := Pixel 8
+PRODUCT_BRAND := google
+
+BUILD_FINGERPRINT := $(PRODUCT_OVERRIDE_FINGERPRINT)
