ca07f6f43e4b71a8fddd59703c6a1889dbb5797f
Kazuki Hashimoto <kazukih0205@gmail.com>
2024-12-24 14:30:59 +0900

    remove SettingsGoogleFutureFaceEnroll

--- face/device.mk
+++ face/device.mk
@@ -3,10 +3,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Face unlock
-PRODUCT_PACKAGES += \
-    SettingsGoogleFutureFaceEnroll
-
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += device/google/shusky/face/overlay
 
